457dcd126140364b5ff2e72a0a4dd48d020092a8
tatiana <tatiana.pekhtereva@gmail.com>
2019-05-07 20:49:10 +1200

    adding Paging and sorting repository

--- src/main/java/com/tat/ec/explorecali/repo/TourRepository.java
+++ src/main/java/com/tat/ec/explorecali/repo/TourRepository.java
@@ -2,12 +2,15 @@ package com.tat.ec.explorecali.repo;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.tat.ec.explorecali.domain.Tour;
 
-public interface TourRepository extends CrudRepository<Tour, Integer>{
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer>{
 
-	List<Tour> findByTourPackageCode(@Param("code") String code);
+	Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable );
 }

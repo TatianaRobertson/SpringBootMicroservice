32bab152df7dbf2832b3d3fc0eff11c8018021d0
tatiana <tatiana.pekhtereva@gmail.com>
2019-05-08 10:47:34 +1200

    POST method for tours/{tourId}/ratings

--- src/main/java/com/tat/ec/explorecali/web/TourRatingController.java
+++ src/main/java/com/tat/ec/explorecali/web/TourRatingController.java
@@ -5,14 +5,19 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tat.ec.explorecali.domain.Tour;
 import com.tat.ec.explorecali.domain.TourRating;
+import com.tat.ec.explorecali.domain.TourRatingPk;
 import com.tat.ec.explorecali.repo.TourRatingRepository;
 import com.tat.ec.explorecali.repo.TourRepository;
 
@@ -37,6 +42,18 @@ public class TourRatingController {
 		return new RatingDto(tourRating.getScore(), tourRating.getComment(), tourRating.getPk().getCustomerId());
 	}
 	
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createTourRating(@PathVariable(value="tourId") 
+	                             int tourId,
+			                     @RequestBody 
+			                     @Validated 
+			                     RatingDto ratingDto){
+		Tour tour = verifyTour(tourId);
+		tourRatingRepository.save(new TourRating(new TourRatingPk(tour, ratingDto.getCustomerId()), ratingDto.getScore(),  ratingDto.getComment()));
+	
+	}
+	
 	private TourRating verifyTourRating(int tourId, int customerId) throws NoSuchElementException{
 		TourRating rating = tourRatingRepository.findByPkTourIdAndPkCustomerId(tourId, customerId);
 		if(rating == null) {
@@ -45,12 +62,13 @@ public class TourRatingController {
 		return rating;
 	}
 	
-	private Optional<Tour> verifyTour(int tourId) throws NoSuchElementException{
+	private Tour verifyTour(int tourId) throws NoSuchElementException{
 		Optional<Tour> tour = tourRepository.findById(tourId);
-		if(tour == null){
-			 throw new NoSuchElementException("Tour does not exist " + tourId);
+		if(tour.isPresent()){
+			return tour.get(); 
+		}else{
+			throw new NoSuchElementException("Tour does not exist " + tourId); 
 		}
-	  return tour; 
 	}
 	
 	@ResponseStatus(HttpStatus.NOT_FOUND)
